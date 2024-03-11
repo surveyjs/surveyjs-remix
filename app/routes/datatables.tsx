2a2a51351c9393ce2db4f8c2290341e18ac277c0
tsv2013 <tsv2013@noreply.github.com>
2024-03-11 14:58:53 +0300

    Added Datatables route stub

--- app/routes/datatables.tsx
+++ app/routes/datatables.tsx
@@ -1,12 +1,12 @@
 import { MetaFunction } from "@remix-run/node";
 import { ClientOnly } from "remix-utils/client-only";
-// import DashboardTabulator from "../components/DashboardTabulator";
+// import DashboardDatatables from "../components/DashboardTabulator";
 // https://stackblitz.com/edit/remix-run-remix-dbe16z?file=app%2Froutes%2F_index.tsx
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Results Table" },
+    { name: "description", content: "Results Table (IE Support)" },
   ];
 };
 
@@ -15,7 +15,7 @@ export default function Survey() {
       <ClientOnly fallback={null}>
         {() =>
         <div className="flex min-h-screen flex-col items-center p-8">
-          {/* <DashboardTabulator /> */}
+          {/* <DashboardDatatables /> */}
         </div>
       }
       </ClientOnly>
