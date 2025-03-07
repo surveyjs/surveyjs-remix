c7ec884d3005eb653646b788074e593a396ea78d
Dmitry Kuzin <dk981234@gmail.com>
2025-03-07 23:50:10 +0300

    Fix build

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,5 +1,4 @@
 import { MetaFunction } from "@remix-run/node";
-import { ClientOnly } from "remix-utils/client-only";
 import Dashboard from "../components/Dashboard.client";
 // https://stackblitz.com/edit/remix-run-remix-dbe16z?file=app%2Froutes%2F_index.tsx
 
@@ -13,9 +12,7 @@ export const meta: MetaFunction = () => {
 export default function Survey() {
   return (
     <div className="flex min-h-screen flex-col">
-      <ClientOnly fallback={null}>
-          {() => <Dashboard /> }
-      </ClientOnly>
+        <Dashboard />
     </div>
   );
 }
