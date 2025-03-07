2b926f8d4f4bf012d5c3123daef5e9edb6c76987
Dmitry Kuzin <dk981234@gmail.com>
2025-03-08 00:09:30 +0300

    Small refactor

--- app/routes/survey.tsx
+++ app/routes/survey.tsx
@@ -1,5 +1,5 @@
 import { MetaFunction } from "@remix-run/node";
-import SurveyComponent from "../components/Survey.client";
+import SurveyComponent from "../components/Survey";
 // https://stackblitz.com/edit/remix-run-remix-dbe16z?file=app%2Froutes%2F_index.tsx
 
 export const meta: MetaFunction = () => {
@@ -11,8 +11,8 @@ export const meta: MetaFunction = () => {
 
 export default function Survey() {
   return (
-    <div className="flex min-h-screen flex-col items-center p-8">
-      <SurveyComponent />
-    </div>  
+          <div className="flex min-h-screen flex-col items-center p-8">
+            <SurveyComponent />
+          </div>
   );
 }
