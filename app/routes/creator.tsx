7f0453395e00976f351b1b3c5fade4aa5e47f1b8
RomanTsukanov <sergeich16@gmail.com>
2025-04-03 12:36:02 +0400

    Update the code example

--- app/routes/creator.tsx
+++ app/routes/creator.tsx
@@ -1,6 +1,5 @@
 import { MetaFunction } from "@remix-run/node";
-import { ClientOnly } from "remix-utils/client-only";
-import SurveyCreatorComponent from "../components/SurveyCreator";
+import SurveyCreatorComponent from "../components/SurveyCreator.client";
 // https://stackblitz.com/edit/remix-run-remix-dbe16z?file=app%2Froutes%2F_index.tsx
 
 export const meta: MetaFunction = () => {
@@ -12,12 +11,8 @@ export const meta: MetaFunction = () => {
 
 export default function Survey() {
   return (
-      <ClientOnly fallback={null}>
-        {() =>
-          <div className="flex min-h-screen flex-col items-center p-8">
-            <SurveyCreatorComponent />
-          </div>
-        }
-      </ClientOnly>
+    <div className="flex min-h-screen flex-col items-center p-8">
+      <SurveyCreatorComponent />
+    </div>
   );
 }
